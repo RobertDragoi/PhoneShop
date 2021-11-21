bec9d055c55c2dccbc4acc498ef9afce7173fdec
RobertDragoi <robertdragoi20@gmail.com>
2021-11-21 14:36:47 +0200

    New modal styling and logic

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,34 +1,47 @@
-import React,{useContext} from 'react';
-import Modal from 'react-modal';
-import './Modal.css';
-import {Link} from 'react-router-dom';
-import ProductContext from './ProductsState/productContext';
+import React, { useContext } from "react";
+import Modal from "react-modal";
+import "./Modal.css";
+import { Link } from "react-router-dom";
+import ProductContext from "./ProductsState/productContext";
+import OrderContext from "./OrderState/orderContext";
 const Aux = (props) => {
-  const productContext=useContext(ProductContext);
-  const {clearCart}=productContext;
+  const productContext = useContext(ProductContext);
+  const orderContext = useContext(OrderContext);
+  const { clearCart } = productContext;
+  const { order } = orderContext;
   return (
-      <div>
-        <Modal 
-           isOpen={props.show}
-           onRequestClose={props.closeModal}
-           className="Modal"
-           overlayClassName="Overlay"
-        >
-            <div className="center">
-              {props.orderId===null?(<div class="spinner-border text-primary"></div>):(<h3>{`Order ${props.orderId} has been sent! Keep shopping on our site!`}</h3>)}
-              
-              </div>
-              <div className="center">
-              <Link to="/">
-           <button className="btn btn-outline-danger text-uppercase mb-3 px-5" onClick={()=>{props.closeModal(); clearCart(); }}>Close Modal</button>
-           </Link>
-              </div>
-          
-          
-        </Modal>
-      </div>
-    
-  )
-}
+    <div>
+      <Modal
+        isOpen={props.show}
+        onRequestClose={props.closeModal}
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        <div className="modal-container">
+          <div className="center">
+            {order === null ? (
+              <div class="spinner-border text-primary"></div>
+            ) : (
+              <h3>{`Order ${order?._id} has been sent! Keep shopping on our site!`}</h3>
+            )}
+          </div>
+          <div className="center">
+            <Link to="/">
+              <button
+                className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                onClick={() => {
+                  props.closeModal();
+                  clearCart();
+                }}
+              >
+                Close Modal
+              </button>
+            </Link>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
 
-export default Aux
+export default Aux;
