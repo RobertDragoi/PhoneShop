b6138946709b184fe4463aae24bf71ee5e79a2d2
RobertDragoi <robertdragoi20@gmail.com>
2021-11-21 17:46:41 +0200

    Add user's orders to the profile

--- client/src/components/Cart/CartItem.js
+++ client/src/components/Cart/CartItem.js
@@ -1,39 +1,58 @@
-import React,{useContext} from 'react'
-import ProductContext from '../ProductsState/productContext';
+import React, { useContext } from "react";
+import ProductContext from "../ProductsState/productContext";
 export default function CartItem(props) {
-    const productContext=useContext(ProductContext);
-    const {removeProduct,increment,decrement}=productContext;
-    const{_id,title,img,price,count,total}=props.item;
-    
-    return (
-        <div className="row my-2 text-center text-capitalize">
-        <div className="col-10 mx-auto col-lg-2">
-            <img src={img} style={{width:'5rem',heigth:"5rem"}} className="img-fluid"></img>
+  const productContext = useContext(ProductContext);
+  const { removeProduct, increment, decrement } = productContext;
+  const { _id, title, img, price, count, total } = props.item;
+
+  return (
+    <div className="row my-2 text-center text-capitalize">
+      <div className="col-10 mx-auto col-lg-2">
+        <img
+          alt="img"
+          src={img}
+          style={{ width: "5rem", heigth: "5rem" }}
+          className="img-fluid"
+        ></img>
+      </div>
+      <div className="col-10 mx-auto col-lg-2">{title}</div>
+      <div className="col-10 mx-auto col-lg-2">$ {price}</div>
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+          <div>
+            {count === 1 ? (
+              <span disabled className="btn btn-black mx-1">
+                -
+              </span>
+            ) : (
+              <span
+                onClick={() => decrement(_id)}
+                className="btn btn-black mx-1"
+              >
+                -
+              </span>
+            )}
+
+            <span className="btn btn-black mx-1">{count}</span>
+            <span onClick={() => increment(_id)} className="btn btn-black mx-1">
+              +
+            </span>
+          </div>
         </div>
-        <div className="col-10 mx-auto col-lg-2">
-            {title}
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <div className="cart-icon">
+          <i
+            onClick={() => {
+              removeProduct(_id);
+            }}
+            className="fas fa-trash"
+          ></i>
         </div>
-        <div className="col-10 mx-auto col-lg-2">
-            $ {price}
-        </div>
-        <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
-            <div className="d-flex justify-content-center">
-            <div>
-            {count===1?(<span disabled className="btn btn-black mx-1" >-</span>):(<span onClick={()=>decrement(_id)} className="btn btn-black mx-1" >-</span>)}
-            
-            <span className="btn btn-black mx-1" >{count}</span>
-            <span  onClick={()=>increment(_id)}className="btn btn-black mx-1" >+</span>
-            </div>  
-            </div>
-        </div>
-        <div className="col-10 mx-auto col-lg-2">
-            <div className="cart-icon" >
-                <i onClick={()=>{removeProduct(_id)}} className="fas fa-trash"></i>
-            </div>
-        </div>
-        <div className="col-10 mx-auto col-lg-2">
-            <strong> item total : $ {total} </strong>
-        </div>
-        </div>
-    )
+      </div>
+      <div className="col-10 mx-auto col-lg-2">
+        <strong> item total : $ {total} </strong>
+      </div>
+    </div>
+  );
 }
--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,5 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import UserContext from "../UserState/userContext";
+import OrderContext from "../OrderState/orderContext";
 import "./Profile.scss";
 const Profile = () => {
   const convertDate = (dateStr) => {
@@ -7,7 +8,16 @@ const Profile = () => {
     return date.toDateString();
   };
   const userContext = useContext(UserContext);
+  const orderContext = useContext(OrderContext);
   const { user } = userContext;
+  const { userOrders, getUserOrders } = orderContext;
+  useEffect(() => {
+    const getUserOrdersAPI = async (id) => {
+      await getUserOrders(id);
+    };
+    getUserOrdersAPI(user?._id);
+  }, [user?._id]);
+
   return (
     <div class="container">
       <div class="row">
@@ -21,12 +31,48 @@ const Profile = () => {
               <div>Date of registration</div>
             </div>
             <div className="profile-container-row">
-              <div>{user.name}</div>
-              <div>{user.age}</div>
-              <div>{user.address}</div>
-              <div>{convertDate(user.date)}</div>
+              <div>{user?.name}</div>
+              <div>{user?.age}</div>
+              <div>{user?.address}</div>
+              <div>{convertDate(user?.date)}</div>
             </div>
           </div>
+          {userOrders.map((order) => (
+            <div className="profile-container">
+              <div className="profile-container-item">
+                {order.products.map((product) => (
+                  <div className="row my-2 text-center text-capitalize">
+                    <div className="col-10 mx-auto col-lg-2">
+                      <img
+                        alt="img"
+                        src={product.product.img}
+                        style={{ width: "5rem", heigth: "5rem" }}
+                        className="img-fluid"
+                      ></img>
+                    </div>
+                    <div className="col-10 mx-auto col-lg-2">
+                      {product.product.title}
+                    </div>
+
+                    <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+                      <div className="d-flex justify-content-center">
+                        <div className="col-10 mx-auto col-lg-2">
+                          <strong> Bucăți : {product.count} </strong>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-10 mx-auto col-lg-2">
+                      <strong> Total produs : {product.total} Lei</strong>
+                    </div>
+                  </div>
+                ))}
+                <div className="col-10 mx-auto col-lg-2">
+                  <strong> Total : {order.price} Lei</strong>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
         <div className="col-2"></div>
       </div>
