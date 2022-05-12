23996439f4f9c23ea0becc32318cf5d7a885cfc1
RobertDragoi <robertdragoi20@gmail.com>
2022-05-12 18:10:53 +0300

    Refactor Input component

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -6,24 +6,24 @@ const RegisterInput = ({
   value,
   type,
   placeholder,
+  size,
   ...rest
 }) => {
   return (
-    <div className="form-group col-md-12">
+    <div className={`form-group col-md-${size?size:'12'}`}>
       <label className="control-label" htmlFor={name}>
-      {placeholder}<span className="text-primary">*</span>:
+        {placeholder}
+        <span className="text-primary">*</span>:
       </label>
-      <div>
-        <input
-          onChange={onChange}
-          type={type}
-          className="form-control"
-          name={name}
-          value={value}
-          placeholder={placeholder}
-          {...rest}
-        />
-      </div>
+      <input
+        onChange={onChange}
+        type={type}
+        className="form-control"
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        {...rest}
+      />
     </div>
   );
 };
